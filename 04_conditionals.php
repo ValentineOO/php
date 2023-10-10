da25cc29ef0e1d8cb59cd9f62f152ca1b4a54dc7
ValentineOO <varlomxg@gmail.com>
2023-10-10 16:56:47 +0100

    php going on well so far

--- 04_conditionals.php
+++ 04_conditionals.php
@@ -13,4 +13,53 @@ $age = 17;
 
 $t = date("H");
 
-echo $t;
+// echo $t;
+
+// if ($t < 12) {
+//     echo "Good Morning";
+// }
+
+// elseif ($t < 17) {
+//     echo "Good afternoon";
+// }
+
+// else {
+//     echo "Good Evening";
+// }
+
+$posts = ["First Posts"];
+
+// if (!empty($posts)) {
+//     echo $posts[0];
+// }
+// else {
+//     echo "No posts";
+// }
+
+// echo !empty($posts) ? $posts[0] : "No Posts";
+
+// $first_Post = !empty($posts) ? $posts[0] : "No Posts";
+// $first_Post = !empty($posts) ? $posts[0] : null;
+
+// Null Coalescing Operator
+// $first_Post = $posts[0] ?? null;
+
+// echo $first_Post;
+
+// switch
+
+$favcolor = "red";
+
+switch ($favcolor) {
+    case "red":
+        echo "Your favorite color is red";
+        break;
+     case 'blue':
+        echo "Your favorite color is blue";
+        break;
+    case 'green':
+        echo "Your favorite color is green";
+        break;
+    default:
+    echo " Your favorite color is not red, green or blue";
+}
---
+++ 05_loops.php
@@ -0,0 +1,36 @@
+<?php
+// for ($x = 0; $x <= 10; $x++) {
+//     echo "Number" . $x . "<br>";
+// }
+
+
+// $x = 1;
+
+// while ($x <= 15) {
+// echo "Number" . $x . "<br>";
+// $x++;
+// }
+
+// do while loop
+
+// $x = 1;
+
+// do {
+//     echo "Number" . $x . "<br>";
+//     $x++;
+// } 
+// while ($x <= 5);
+
+$posts = ["first post", "Second post", "third post"];
+
+// for ($x =0; $x < count($post); $x++) {
+//     echo $post[$x];
+// }
+
+// foreach($posts as $post) {
+//     echo $post;
+// }
+
+// foreach($posts as $index => $post) {
+//     echo $index. "-" .  $post . "<br>";
+// }
